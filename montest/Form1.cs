bf75a02d6b937fe70d3a08762ecc94c349aac1c4
Annie <gitannie@annie.pourriere@gmail.com>
2016-09-20 23:20:00 +0200

    v0

--- montest/Form1.cs
+++ montest/Form1.cs
@@ -16,5 +16,11 @@ namespace montest
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("1er bouton");
+
+        }
     }
 }
